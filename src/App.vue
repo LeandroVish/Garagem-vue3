d8526096dfbf00a975ff6c0b224eb95ca09d42f2
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-04-02 14:48:45 -0300

    up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
+
+import router from './router';
 import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
+    <router-link to="/acessorios">Acessorios</router-link> |
     <router-link to="/categorias">Categorias</router-link> |
-    <router-link to="/acessorios">Acessorio</router-link>
+    <router-link to="/cores">Cores</router-link> |
   </header>
   <main>
     <RouterView />
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import CategoriaView from "../views/CategoriaView.vue";
 import AcessoriosView from "../views/AcessoriosView.vue";
+import CategoriaView from "../views/CategoriaView.vue";
+import CorView from "../views/CorView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,14 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessoriosView,
+    },
+    {
       path: "/categorias",
       name: "categorias",
       component: CategoriaView,
     },
     {
-      path: "/acessorios",
-      name: "acessorios",
-      component: AcessoriosView,
+      path: "/cor",
+      name: "cores",
+      component: CorView,
     },
   ],
 });
